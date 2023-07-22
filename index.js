20d4d5ca34b7fd8249cb4d98297a499f6e117ced
danusorn23456 <danusorn23456@gmail.com>
2023-07-22 16:55:50 +0700

    initial new version

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nbt = require('prismarine-nbt');
+const multer = require("multer")
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.get('/', (req, res) => {
+    res.send("<h1>welcome to blueprint mc api</h1>")
+});
+
+app.post("/upload", upload.single('nbt_file'), async (req, res) => {
+    const parsedNbt = await nbt.parse(req.file.buffer).then(d => d.parsed)
+
+    res.json(parsedNbt)
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
